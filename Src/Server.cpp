ee1f74619f6420a7393c28e68c2e421af567452d
mobouifr <125811900+mobouifr@users.noreply.github.com>
2025-08-21 19:14:52 +0000

    Initialised poll struct and Finished the work on handlePollEvents()

--- Src/Server.cpp
+++ Src/Server.cpp
@@ -47,60 +47,125 @@
 
 // ===== Main control =====
 
-	void Server::run() {
-	std::cout << "inside the run function" << std::endl;
-	_running = true;
-	while(_running) {
+	void Server::run() 
+	{
+		std::cout << "inside the run function" << std::endl;
+		_running = true;
+
 		initListenSocket(); // Initialize the listening socket
-		handlePollEvents(); // Handle events from clients and the server
-	}
-} 
+		if (!_running)
+			return;
+
+		while(_running)
+			handlePollEvents(); // Handle events from clients and the server
+	} 
 	void Server::stop()
 	{
 	}
 
 // ===== Internal helpers =====
-	void Server::initListenSocket() {   
-	int socket_fd;
-	struct sockaddr_in my_addr;
-	socket_fd = socket(AF_INET,SOCK_STREAM,0);
-	std::cout << "socket_fd: " << socket_fd << std::endl;
-	if (socket_fd == -1) {
-		std::cerr << "Error creating socket" << std::endl;
-		_running = false;
+	void Server::initListenSocket() 
+	{   
+		int socket_fd;
+		struct sockaddr_in my_addr;
+
+		socket_fd = socket(AF_INET,SOCK_STREAM,0);
+		std::cout << "socket_fd: " << socket_fd << std::endl;
+		if (socket_fd == -1)
+		{
+			std::cerr << "Error creating socket" << std::endl;
+			_running = false;
+		}
+		
+		my_addr.sin_family = AF_INET;
+		std::cout << "Debugging | sin_famly is =  " << my_addr.sin_family << std::endl;
+		my_addr.sin_port = htons(3490);
+		std::cout << "Debugging | sin_port is = " << my_addr.sin_port << std::endl;
+		my_addr.sin_addr.s_addr = INADDR_ANY;
+		std::cout << "Debugging | sin_addr is = " << my_addr.sin_addr.s_addr << std::endl;
+		my_addr.sin_zero[0] = '\0';
+		std::cout << "Debugging | sin_zero is = " << my_addr.sin_zero[0] << std::endl;
+		
+		fcntl(socket_fd,F_SETFL,O_NONBLOCK);
+		
+		//setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &(int){0}, sizeof(int));
+		
+		int bind_result;
+		_listen_fd = socket_fd;
+		bind_result = bind(socket_fd, (struct sockaddr*)&my_addr, sizeof(my_addr));
+		if (bind_result == -1)
+		{
+			std::cerr << "Error binding socket" << std::endl;
+			_running = false;
+			return;
+		}
+		std::cout << "Socket bound successfully" << std::endl;
+		
+		int listen_result;
+		listen_result = listen(socket_fd,50);
+		if (listen_result == -1)
+		{
+			std::cerr << "Error listening on socket" << std::endl;
+			_running = false;
+			return;
+		}
+		
+		// =========== [MB] part start ============
+		struct pollfd	poll_struct;
+		poll_struct.fd = _listen_fd;
+		poll_struct.events = POLLIN;
+		poll_struct.revents = 0;
+		_poll_fds.push_back(poll_struct);
+		// =========== [MB] part end. =========
+		
+		std::cout << "the server is listening" << std::endl; 
+
 	}
-	my_addr.sin_family = AF_INET;
-	std::cout << "Debugging | sin_famly is =  " << my_addr.sin_family << std::endl;
-	my_addr.sin_port = htons(3490);
-	std::cout << "Debugging | sin_port is = " << my_addr.sin_port << std::endl;
-	my_addr.sin_addr.s_addr = INADDR_ANY;
-	std::cout << "Debugging | sin_addr is = " << my_addr.sin_addr.s_addr << std::endl;
-	my_addr.sin_zero[0] = '\0';
-	std::cout << "Debugging | sin_zero is = " << my_addr.sin_zero[0] << std::endl;
-	fcntl(socket_fd,F_SETFL,O_NONBLOCK);
-	//setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &(int){0}, sizeof(int));
-	int bind_result;
-	_listen_fd = socket_fd;
-	bind_result = bind(socket_fd, (struct sockaddr*)&my_addr, sizeof(my_addr));
-	if (bind_result == -1) {
-		std::cerr << "Error binding socket" << std::endl;
-		_running = false;
-		return;
-	}
-	std::cout << "Socket bound successfully" << std::endl;
-	int listen_result;
-	listen_result = listen(socket_fd,50);
-	if (listen_result == -1) {
-		std::cerr << "Error listening on socket" << std::endl;
-		_running = false;
-		return;
-	}
-	std::cout << "the server is listening" << std::endl;
-}
+
+	void Server::handlePollEvents()
+	{
+		if (_poll_fds.empty())
+			return ;
+
+		int timeout_ms = 1000; // wait 1000ms = 1s.
+		int ready = poll(&_poll_fds[0], _poll_fds.size(), timeout_ms);
+
+		if (ready == -1)
+		{
+			std::cerr << "poll failed: ";
+			perror("poll");
+			_running = false;
+			return ;
+		}
+
+		if (ready == 0)
+			return ;
+
+		
+		for (int i = 0; i < _poll_fds.size(); i++)
+		{
+			int fd = _poll_fds[i].fd;
+			int result_event = _poll_fds[i].revents;
+			
+			if (result_event == 0) // no events.
+				continue ;
+
+			if (fd == _listen_fd && (result_event & POLLIN)) // 1. New connection on listening socket
+			{
+				acceptNewClient();
+				continue ;
+			}
+			if (result_event & POLLIN) // 2. Existing client sent data
+				handleClientRead(fd);
+			if (result_event & POLLOUT)// 3. Existing client ready to send
+				handleClientWrite(fd);
+			if (re & (POLLER | POLLHUP | POLLNVAL)) // 4. Error or disconnection
+				removeClient(fd);
+		}
+	} 
 
 	void Server::acceptNewClient() { /* TO DO; */ } 
 	void Server::removeClient(int fd) { (void)fd; /* TO DO; */ } 
-	void Server::handlePollEvents() { /* TO DO; */ } 
 	void Server::handleClientRead(int fd) { (void)fd; /* TO DO; */ }
 	void Server::handleClientWrite(int fd) { (void)fd; /* TO DO; */ } 
 	void Server::sendToFd(int fd, const std::string& msg) { (void)fd; (void)msg; /* TO DO; */ }
